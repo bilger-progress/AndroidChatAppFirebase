6bbb3601c86fea2cd02cebea32d4e3c303127f5a
bilger-progress <bilger.yahov@progress.com>
2018-12-06 13:44:43 +0200

    Save the display name locally using SharedPreferences.

--- app/src/main/java/com/yahov/flashchatnewfirebase/RegisterActivity.java
+++ app/src/main/java/com/yahov/flashchatnewfirebase/RegisterActivity.java
@@ -1,17 +1,16 @@
 package com.yahov.flashchatnewfirebase;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -109,12 +108,21 @@ public class RegisterActivity extends AppCompatActivity {
                 if (!task.isSuccessful()) {
                     Log.d("FlashChat", "user creation failed");
                     showErrorDialog("Registration attempt failed!");
+                } else {
+                    saveDisplayName();
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    finish();
+                    startActivity(intent);
                 }
             }
         });
     }
 
-    // TODO: Save the display name to Shared Preferences
+    private void saveDisplayName () {
+        String displayName = mUsernameView.getText().toString();
+        SharedPreferences prefs = getSharedPreferences(CHAT_PREFS, 0);
+        prefs.edit().putString(DISPLAY_NAME_KEY, displayName).apply();
+    }
 
     private void showErrorDialog (String message) {
         new AlertDialog.Builder(this)
